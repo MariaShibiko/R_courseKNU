1d0802a529b2f19e2b9295aed0f84374fccc19df
MariaShibiko <49059056+MariaShibiko@users.noreply.github.com>
2019-12-15 20:11:02 +0200

    Update run_analysis.R

--- Lab5/run_analysis.R
+++ Lab5/run_analysis.R
@@ -4,13 +4,13 @@ library(dplyr)
 
 #4. Відповідно присвоює змінним у наборі даних описові імена. 
 
-df_label <- read.table('UCI HAR Dataset\\features.txt', colClasses = 'character')[,2]
+df_labels <- read.table('UCI HAR Dataset\\features.txt', colClasses = 'character')[,2]
 
-x_train <- read.table('UCI HAR Dataset\\train\\X_train.txt', col.names = df_label, check.names = F)
+x_train <- read.table('UCI HAR Dataset\\train\\X_train.txt', col.names = df_labels, check.names = F)
 y_train <- read.table('UCI HAR Dataset\\train\\y_train.txt', col.names = c('Activities'))
 subj_train <- read.table('UCI HAR Dataset\\train\\subject_train.txt', col.names = c('Subject'))
 
-x_test <- read.table('UCI HAR Dataset\\test\\X_test.txt', col.names = df_label, check.names = F)
+x_test <- read.table('UCI HAR Dataset\\test\\X_test.txt', col.names = df_labels, check.names = F)
 y_test <- read.table('UCI HAR Dataset\\test\\y_test.txt', col.names = c('Activities'))
 subj_test <- read.table('UCI HAR Dataset\\test\\subject_test.txt', col.names = c('Subject'))
 
@@ -22,19 +22,19 @@ df <- cbind(rbind(x_train, x_test), rbind(y_train, y_test), rbind(subj_train, su
 
 #2. Витягує лише вимірювання середнього значення та стандартного відхилення (mean and standard deviation) для кожного вимірювання. 
 
-statistics <- select(df,  matches("mean\\(\\)|std\\(\\)|Subject|Activity"))
+stat <- df[ , grep("-mean\\(\\)|-std\\(\\)", colnames(df))]
 
 #3. Використовує описові назви діяльностей (activity) для найменування діяльностей у наборі даних. 
 
-df3 <- within(statistics, Activity <- factor(activity, labels = activity_labels[,2]))
+df_ <- within(stat, Activity <- factor(activity, labels = activity_labels[,2]))
 
-colnames(df3) <- gsub("^t", "time", colnames(df3))
-colnames(df3) <- gsub("^f", "frequency", colnames(df3))
-colnames(df3) <- gsub("Acc", "Accelerometer", colnames(df3))
-colnames(df3) <- gsub("Gyro", "Gyroscope", colnames(df3))
-colnames(df3) <- gsub("Mag", "Magnitude", colnames(df3))
+colnames(df_) <- gsub("^t", "time", colnames(df_))
+colnames(df_) <- gsub("^f", "frequency", colnames(df_))
+colnames(df_) <- gsub("Acc", "Accelerometer", colnames(df_))
+colnames(df_) <- gsub("Gyro", "Gyroscope", colnames(df_))
+colnames(df_) <- gsub("Mag", "Magnitude", colnames(df_))
 
 #5. З набору даних з кроку 4 створити другий незалежний акуратний набір даних (tidy dataset) із середнім значенням для кожної змінної для кожної діяльності та кожного суб’єкту (subject). 
-df_final <- aggregate(x = df3[, -c(67,68)], by = list(df3[,'subject'], df3[, 'Activity']), FUN = mean)
+final <- aggregate(x = df_[, -c(67,68)], by = list(df_[,'subject'], df_[, 'Activity']), FUN = mean)
 
-write.csv(df_final, "tidy_dataset.csv", row.names=F)
+write.csv(final, "tidy_dataset.csv", row.names=F)
